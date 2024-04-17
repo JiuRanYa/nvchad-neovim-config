427364c23c77dbd32a6d27fbd84ffb4d89df82b7
JiuRanYa <1084350607@qq.com>
2024-04-17 14:16:28 +0800

    fix: vue language server with tsserver

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,6 +6,10 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local servers = { "html", "cssls", "volar", "eslint", "tailwindcss" }
 
+local mason_registry = require("mason-registry")
+local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+	.. "/node_modules/@vue/language-server"
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
@@ -22,15 +26,20 @@ lspconfig.eslint.setup({
 })
 
 lspconfig.tsserver.setup({
-	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "json" },
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "json", "vue" },
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+			},
+		},
+	},
 })
 
 lspconfig.volar.setup({
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-	init_options = {},
 	capabilities = {
 		workspace = {
 			didChangeWatchedFiles = {
