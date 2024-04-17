cf1d89908ff138f77ab6438cef7475c32689ec00
JiuRanYa <1084350607@qq.com>
2024-04-17 16:27:25 +0800

    fix: vue language server tsserver path

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -10,6 +10,26 @@ local mason_registry = require("mason-registry")
 local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
 	.. "/node_modules/@vue/language-server"
 
+local util = require("lspconfig.util")
+local function get_typescript_server_path(root_dir)
+	-- local global_ts = "/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib"
+	local global_ts = "/Users/jiuran/.config/nvm/versions/node/v20.11.1/lib/node_modules/typescript/lib"
+	-- Alternative location if installed as root:
+	-- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+	local found_ts = ""
+	local function check_dir(path)
+		found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+		if util.path.exists(found_ts) then
+			return path
+		end
+	end
+	if util.search_ancestors(root_dir, check_dir) then
+		return found_ts
+	else
+		return global_ts
+	end
+end
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
@@ -47,4 +67,12 @@ lspconfig.volar.setup({
 			},
 		},
 	},
+	on_new_config = function(new_config, new_root_dir)
+		new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+	end,
+	-- init_options = {
+	-- 	vue = {
+	-- 		hybridMode = false,
+	-- 	},
+	-- },
 })
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -10,24 +10,24 @@ map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 local keymap = vim.keymap.set
 
-local hop = require('hop')
-local directions = require('hop.hint').HintDirection
+local hop = require("hop")
+local directions = require("hop.hint").HintDirection
 
 function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- 设置jj退出insert
-map('i', 'jj', '<Esc>', {noremap = true})
+map("i", "jj", "<Esc>", { noremap = true })
 
 -- 刷新lua配置
-map('n', '<leader>rs', ':luafile ~/.config/nvim/init.lua<CR>', {noremap = true})
+map("n", "<leader>rs", ":luafile ~/.config/nvim/init.lua<CR>", { noremap = true })
 
-map('n', ';', ':noh<CR>')
+map("n", ";", ":noh<CR>")
 -- nvim-tree
 map("n", "<C-b>", ":NvimTreeToggle<CR>", {}) -- open/close
 map("n", "<C-f>", ":NvimTreeFindFile<CR>", {}) -- open/close
@@ -37,18 +37,18 @@ map("n", "<S-h>", ":BufferLineCyclePrev<CR>", opt)
 map("n", "<S-l>", ":BufferLineCycleNext<CR>", opt)
 
 -- 关闭当前缓冲区
-map('n', '<leader>bd', ':BufferLineClose<CR>', opts)
+map("n", "<leader>bd", ":BufferLineClose<CR>", opts)
 
 -- 关闭除当前缓冲区外的所有缓冲区
-map('n', '<leader>bD', ':BufferLineCloseAllButCurrent<CR>', opts)
+map("n", "<leader>bD", ":BufferLineCloseAllButCurrent<CR>", opts)
 
 -- 窗口切换
 map("n", "<leader>h", "<C-w>h", opt)
 map("n", "<leader>l", "<C-w>l", opt)
 
 -- 窗口分屏
-map('n', '<leader>s', ':vsplit<CR>', opt)
-map('n', '<leader>v', ':split<CR>', opt)
+map("n", "<leader>s", ":vsplit<CR>", opt)
+map("n", "<leader>v", ":split<CR>", opt)
 vim.keymap.set("n", "<S-Right>", ":vertical resize +5<CR>", opt)
 vim.keymap.set("n", "<S-Left>", ":vertical resize -5<CR>", opt)
 vim.keymap.set("n", "<S-Up>", ":resize +5<CR>", opt)
@@ -57,14 +57,14 @@ vim.keymap.set("n", "<S-Up>", ":resize +5<CR>", opt)
 map("n", "<C-s>", ":w<CR>", opt)
 
 -- lazy git
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 -- lspsaga
 -- keymap("n", "[E", function() require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, { silent = true })
 -- keymap("n", "]E", function() require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR }) end, { silent = true })
-keymap("n","<leader>O", ":Lspsaga outline<CR>")
+keymap("n", "<leader>O", ":Lspsaga outline<CR>")
 keymap("n", "K", ":Lspsaga hover_doc<CR>", { silent = true })
-keymap({"n","v"}, "<leader>ca", ":Lspsaga code_action<CR>", { silent = true })
+keymap({ "n", "v" }, "<leader>ca", ":Lspsaga code_action<CR>", { silent = true })
 keymap("n", "rn", ":Lspsaga rename<CR>", { silent = true })
 keymap("n", "gd", ":Lspsaga peek_definition<CR>", { silent = true })
 keymap("n", "fd", ":Lspsaga lsp_finder<CR>", { silent = true })
@@ -74,7 +74,9 @@ keymap("n", "<leader>,", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = tru
 keymap("n", "<leader>.", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 
 -- Comment
-keymap("n", "<leader>/", function() require("Comment.api").toggle.linewise.current() end)
+keymap("n", "<leader>/", function()
+	require("Comment.api").toggle.linewise.current()
+end)
 
 -- format
 -- keymap("n", "<C-s>", function() require("conform").format({ bufnr = args.buf }) end, opt)
@@ -84,23 +86,23 @@ map("i", "<C-l>", "<RIGHT>")
 map("i", "<C-h>", "<LEFT>")
 
 -- close tab for buffer line
-keymap({"n","v"}, "<leader>cb", ":bdelet<CR>")
+keymap({ "n", "v" }, "<leader>cb", ":bdelet<CR>")
 
 -- motion with hop
 keymap("n", "<leader>w", ":HopWordMW<CR>", opt)
-keymap('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, {remap=true})
-keymap('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, {remap=true})
-keymap('', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, {remap=true})
-keymap('', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, {remap=true})
+keymap("", "f", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, { remap = true })
+keymap("", "F", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, { remap = true })
+keymap("", "t", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, { remap = true })
+keymap("", "T", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, { remap = true })
 
 keymap("n", "[c", function()
-  require("treesitter-context").go_to_context()
+	require("treesitter-context").go_to_context()
 end, { silent = true })
