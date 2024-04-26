163ce045591002c411b4b6e14d172e858b5fdd20
JiuRanYa <1084350607@qq.com>
2024-04-26 17:14:58 +0800

    feat: support cargo silicon

---
+++ lua/plugins/codescreen.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"segeljakt/vim-silicon",
+		lazy = false,
+	},
+	-- {
+	-- 	"michaelrommel/nvim-silicon",
+	-- 	lazy = true,
+	-- 	cmd = "Silicon",
+	-- 	config = function()
+	-- 		require("silicon").setup({
+	-- 			-- Configuration here, or leave empty to use defaults
+	-- 			font = "Fira Code",
+	-- 			to_clipboard = true,
+	-- 			-- run cargo install bat * bat --list-themes
+	-- 			theme = "Dracula",
+	-- 		})
+	-- 	end,
+	-- },
+}
