4625f0660ae1617cbc06c4f4732a102d5d363c8e
JiuRanYa <1084350607@qq.com>
2024-05-07 11:45:34 +0800

    feat: code screen

--- lua/plugins/codescreen.lua
+++ lua/plugins/codescreen.lua
@@ -1,20 +1,47 @@
 return {
-	{
-		"segeljakt/vim-silicon",
-		lazy = false,
-	},
 	-- {
-	-- 	"michaelrommel/nvim-silicon",
-	-- 	lazy = true,
+	-- 	"segeljakt/vim-silicon",
+	-- 	lazy = false,
+	-- },
+	-- {
+	-- 	"krivahtoo/silicon.nvim",
+	-- 	build = "./install.sh build",
+	--
 	-- 	cmd = "Silicon",
 	-- 	config = function()
 	-- 		require("silicon").setup({
-	-- 			-- Configuration here, or leave empty to use defaults
+	-- 			-- /Fira Code;
 	-- 			font = "Fira Code",
 	-- 			to_clipboard = true,
 	-- 			-- run cargo install bat * bat --list-themes
 	-- 			theme = "Dracula",
+	-- 			output = {
+	-- 				path = "~/Desktop/obs/",
+	-- 			},
+	-- 			window_title = function()
+	-- 				return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ":~:.")
+	-- 			end,
 	-- 		})
 	-- 	end,
 	-- },
+	{
+		"michaelrommel/nvim-silicon",
+		lazy = true,
+		cmd = "Silicon",
+		config = function()
+			require("silicon").setup({
+				-- /Fira Code;
+				font = "Fira Code;简宋",
+				to_clipboard = true,
+				-- run cargo install bat * bat --list-themes
+				theme = "Dracula",
+				output = function()
+					return "~/Desktop/obs"
+				end,
+				window_title = function()
+					return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ":~:.")
+				end,
+			})
+		end,
+	},
 }
