68ee667bb07b9b4dec702b565a051776466295c2
JiuRanYa <1084350607@qq.com>
2024-05-27 14:53:38 +0800

    chore: compact with nvchad

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
-		config = function()
+		opts = function()
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
 
--- lua/plugins/winbar.lua
+++ lua/plugins/winbar.lua
@@ -2,7 +2,7 @@ return {
 	{
 		event = "VeryLazy",
 		"fgheng/winbar.nvim",
-		config = function()
+		opts = function()
 			require("winbar").setup({
 				enabled = true,
 
